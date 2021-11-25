2d04dd5b45c43727907a78d6e89b6e4d34a0a211
Johan Chavaillaz <j.chavaillaz@gmail.com>
2021-11-26 00:38:40 +0100

    Correct index configuration test.

--- src/test/java/com/chavaillaz/appender/log4j/ElasticsearchConfigurationTest.java
+++ src/test/java/com/chavaillaz/appender/log4j/ElasticsearchConfigurationTest.java
@@ -2,6 +2,10 @@ package com.chavaillaz.appender.log4j;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static java.time.OffsetDateTime.now;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ElasticsearchConfigurationTest {
@@ -9,15 +13,19 @@ class ElasticsearchConfigurationTest {
     @Test
     void testGenerateIndexNameDate() {
         // Given
+        String index = "idx";
+        String suffix = "yyyy.MM.dd";
         ElasticsearchConfiguration configuration = new ElasticsearchConfiguration();
-        configuration.setIndex("idx");
-        configuration.setIndexSuffix("-yyyy.MM.dd");
+        configuration.setIndex(index);
+        configuration.setIndexSuffix("-" + suffix);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+        OffsetDateTime date = now();
 
         // When
-        String indexName = configuration.generateIndexName("2021-01-20T16:30:15.791");
+        String indexName = configuration.generateIndexName(date.toString());
 
         // Then
-        assertThat(indexName).isEqualTo("idx-2021.01.20");
+        assertThat(indexName).isEqualTo(index + "-" + formatter.format(date));
     }
 
 }
