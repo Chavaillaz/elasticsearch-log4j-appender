037133f3b5823878afef461f5ecfdeaaae79ab2b
Johan Chavaillaz <j.chavaillaz@gmail.com>
2023-02-11 12:28:05 +0100

    Update dependencies.

--- src/test/java/com/chavaillaz/appender/log4j/ElasticsearchAppenderTest.java
+++ src/test/java/com/chavaillaz/appender/log4j/ElasticsearchAppenderTest.java
@@ -32,7 +32,7 @@ class ElasticsearchAppenderTest {
     public static final String ELASTICSEARCH_PASSWORD = "changeme";
     public static final DockerImageName ELASTICSEARCH_IMAGE = DockerImageName
             .parse("docker.elastic.co/elasticsearch/elasticsearch")
-            .withTag("8.6.0");
+            .withTag("8.6.1");
 
     protected static ElasticsearchAppender createAppender(String application, String hostname, String elastic, boolean parallel) {
         ElasticsearchAppender.Builder builder = ElasticsearchAppender.builder();
